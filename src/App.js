6745492bef4a25f4630e24dae2aaea8487252ec9
Nicholas Myers <nicholas.myers.porfessional@gmail.com>
2020-04-15 13:25:54 -0700

    pulled all info and layed out and styled the page

--- src/App.js
+++ src/App.js
@@ -1,29 +1,39 @@
-import React, { useState, UseEffect, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "./App.css";
 
 function App() {
-  const [nasaDailyData, setNasaDailyData] = useState(null)
+  const [nasaDailyData, setNasaDailyData] = useState(null);
 
-  // useEffect(() => {
-  //   axios.get("https://api.nasa.gov/planetary/apod?api_key=xf0GPFzVbb5SzSpoHha4wjo7igdUoiG0uejdqLFO")
-  //   .then(res => {
-  //     setNasaDailyData(res.data)
-  //   })
-  //   .catch(err => {
-  //     console.log(err)
-  //   })
-  // }, [])
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.nasa.gov/planetary/apod?api_key=xf0GPFzVbb5SzSpoHha4wjo7igdUoiG0uejdqLFO"
+      )
+      .then((res) => {
+        setNasaDailyData(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
-  console.log(nasaDailyData)
+  // console.log(nasaDailyData)
   // https://apod.nasa.gov/apod/image/2004/MVP_Aspinall_960.jpg
   // api key=xf0GPFzVbb5SzSpoHha4wjo7igdUoiG0uejdqLFO
 
   return (
     <div className="App">
-      <div className="NASAcard">
-        <img src="nasaDailyData.url"></img>
-      </div>
+      <header><h1>NASA PHOTO OF THE DAY</h1></header>
+      <section>
+        <div className="NASAcard">
+          {nasaDailyData && <h2>{nasaDailyData.title}</h2>}
+          {nasaDailyData && <img src={nasaDailyData.url} />}
+          {nasaDailyData && <p>{nasaDailyData.explanation}</p>}
+          
+        </div>
+      </section>
+      <foooter>{nasaDailyData && <p>{nasaDailyData.copyright} {nasaDailyData.date}</p>}</foooter>
     </div>
   );
 }
