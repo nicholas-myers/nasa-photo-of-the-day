c111a6bcea592e76ea5412e92a9805c61196b217
Nicholas Myers <nicholas.myers.porfessional@gmail.com>
2020-04-15 12:31:43 -0700

    able to pull data but not able to get picture to display

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
-import React from "react";
+import React, { useState, UseEffect, useEffect} from "react";
+import axios from "axios";
 import "./App.css";
 
 function App() {
+  const [nasaDailyData, setNasaDailyData] = useState(null)
+
+  // useEffect(() => {
+  //   axios.get("https://api.nasa.gov/planetary/apod?api_key=xf0GPFzVbb5SzSpoHha4wjo7igdUoiG0uejdqLFO")
+  //   .then(res => {
+  //     setNasaDailyData(res.data)
+  //   })
+  //   .catch(err => {
+  //     console.log(err)
+  //   })
+  // }, [])
+
+  console.log(nasaDailyData)
+  // https://apod.nasa.gov/apod/image/2004/MVP_Aspinall_960.jpg
+  // api key=xf0GPFzVbb5SzSpoHha4wjo7igdUoiG0uejdqLFO
+
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+      <div className="NASAcard">
+        <img src="nasaDailyData.url"></img>
+      </div>
     </div>
   );
 }
