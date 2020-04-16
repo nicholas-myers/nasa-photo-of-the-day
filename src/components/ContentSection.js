5f680102c0b80508b8294f86ce50a02878f11ddf
Nicholas Myers <nicholas.myers.porfessional@gmail.com>
2020-04-16 14:05:36 -0700

    added some cool positoning and opacity

--- src/components/ContentSection.js
+++ src/components/ContentSection.js
@@ -2,21 +2,47 @@ import React from "react";
 import styled from "styled-components";
 // import "../../src/App.css";
 
+const StyleContentSection = styled.section`
+  max-height: 100%;
+  width:100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-flow: column;
+`
+
 const StyleNASAimg = styled.img`
-  width: 700px;
-  height: 500px;
+  height: 100%;
+  width: 100%;
+  
 `;
+
+const StyleNASAcard = styled.div`
+  position: absolute;
+  border: 1px solid white;
+  background-color: navy;
+  padding: 1rem;
+  border-radius: 1rem;
+  width: 80%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-flow: column;
+  opacity: 50%;
+`;
+
 
 function ContentSection(props) {
   const { title, url, explanation } = props;
   return (
-    <section className="content">
-      <div className="NASAcard">
+    <StyleContentSection>
+      <StyleNASAimg src={url} />
+      <StyleNASAcard>
         <h2>{title}</h2>
-        <StyleNASAimg src={url} />
+        
         <p>{explanation}</p>
-      </div>
-    </section>
+      </StyleNASAcard>
+    </StyleContentSection>
   );
 }
 
